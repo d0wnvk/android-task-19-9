f7e4b876dff842aec8b928adb47548084b15c2a9
Korolevsky Vladimir <korolevskiy.v.a@sberbank.ru>
2021-07-09 09:21:36 +0300

    first commit

---
+++ app/src/main/java/com/vlad/snake/android_task_19_9/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vlad.snake.android_task_19_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
